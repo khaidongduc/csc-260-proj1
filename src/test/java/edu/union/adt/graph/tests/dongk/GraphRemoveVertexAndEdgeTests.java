1be9197a0d293008ef4120a4db0c267e67db9b65
Khai Dong <dongk@union.edu>
2023-01-13 11:12:58 -0500

    add test GraphRemoveVertexAndEdgeTests.removeNonIsolatedVertex()

--- src/test/java/edu/union/adt/graph/tests/dongk/GraphRemoveVertexAndEdgeTests.java
+++ src/test/java/edu/union/adt/graph/tests/dongk/GraphRemoveVertexAndEdgeTests.java
@@ -54,4 +54,28 @@ public class GraphRemoveVertexAndEdgeTests {
         assertTrue("Graph should still contains edge 2 5", g.hasEdge(new String("2"), new String("5")));
         assertTrue("Graph should still contains edge 5 1", g.hasEdge(new String("5"), new String("1")));
     }
+
+    @Test
+    public void removeNonIsolatedVertex(){
+        g.removeVertex(new String("1"));
+
+        assertFalse("Graph no longer contain vertex 1", g.contains(new String("1")));
+        assertTrue("Graph should still contains vertex 2", g.contains(new String("2")));
+        assertTrue("Graph should still contains vertex 3", g.contains(new String("3")));
+        assertTrue("Graph should still contains vertex 4", g.contains(new String("4")));
+        assertTrue("Graph should still contains vertex 5", g.contains(new String("5")));
+        assertTrue("Graph should still contains vertex 6", g.contains(new String("6")));
+
+        assertFalse("Graph no longer contain edge 1 2", g.hasEdge(new String("1"), new String("2")));
+        assertFalse("Graph no longer contain edge 1 3", g.hasEdge(new String("1"), new String("3")));
+        assertTrue("Graph should still contains edge 3 2", g.hasEdge(new String("3"), new String("2")));
+        assertFalse("Graph no longer contain edge 1 4", g.hasEdge(new String("1"), new String("4")));
+        assertFalse("Graph no longer contain edge 4 1", g.hasEdge(new String("4"), new String("1")));
+        assertTrue("Graph should still contains edge 2 5", g.hasEdge(new String("2"), new String("5")));
+        assertFalse("Graph no longer contain edge 5 1", g.hasEdge(new String("5"), new String("1")));
+
+        assertEquals("Number of vertex reduces by 1", g.numVertices(), 5);
+        assertEquals("Number of vertices reduces by 5", g.numEdges(), 2);
+    }
+
 }
